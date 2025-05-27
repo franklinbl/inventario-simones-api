f912079a9c11b3aea5199bb67a550a8985c3d9bc
franklin barco <franklin@flowrms.com>
2025-05-27 19:07:54 -0400

    Update database configuration and add production start script: Refactor Sequelize initialization to support DATABASE_URL for production environments, and add a new script for starting the application in production mode.

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -4,17 +4,46 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 // Configuración de la conexión a PostgreSQL
-const sequelize = new Sequelize(
-  process.env.DB_NAME!,
-  process.env.DB_USER!,
-  process.env.DB_PASSWORD!,
-  {
-    host: process.env.DB_HOST!,
-    port: parseInt(process.env.DB_PORT!),
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME!,
+//   process.env.DB_USER!,
+//   process.env.DB_PASSWORD!,
+//   {
+//     host: process.env.DB_HOST!,
+//     port: parseInt(process.env.DB_PORT!),
+//     dialect: 'postgres',
+//     logging: false, // Desactiva los logs de Sequelize
+//   }
+// );
+
+let sequelize: Sequelize;
+
+const databaseUrl = process.env.DATABASE_URL;
+
+if (databaseUrl) {
+  sequelize = new Sequelize(databaseUrl, {
     dialect: 'postgres',
-    logging: false, // Desactiva los logs de Sequelize
+    logging: false,
+  });
+} else {
+  // Usamos variables individuales en desarrollo
+  const dbName = process.env.DB_NAME;
+  const dbUser = process.env.DB_USER;
+  const dbPassword = process.env.DB_PASSWORD || "";
+  const dbHost = process.env.DB_HOST;
+  const dbPort = parseInt(process.env.DB_PORT || '5432');
+
+  if (!dbName || !dbUser || !dbHost) {
+    throw new Error('Faltan variables de entorno para la base de datos en desarrollo');
   }
-);
+
+  sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    port: dbPort,
+    dialect: 'postgres',
+    logging: false,
+  });
+}
 
 // Exportar la variable JWT_SECRET
 export const JWT_SECRET = process.env.JWT_SECRET;
