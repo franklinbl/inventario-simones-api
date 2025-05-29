737689f7f2176364f3ed76ab79ec80a44e1034cb
franklin barco <franklin@flowrms.com>
2025-05-28 20:38:11 -0400

    Enhance RentalProduct model with auto-incrementing ID: Add an 'id' field to the RentalProduct model, making it a primary key with auto-increment functionality, and introduce a creation attributes interface for better type handling.

--- src/models/rental-product.model.ts
+++ src/models/rental-product.model.ts
@@ -1,13 +1,17 @@
-import { DataTypes, Model } from 'sequelize';
+import { DataTypes, Model, Optional } from 'sequelize';
 import sequelize from '../config/db.config';
 
 interface RentalProductAttributes {
+  id: number;
   rental_id: number;
   product_id: number;
   quantity: number;
 }
 
-class RentalProduct extends Model<RentalProductAttributes> implements RentalProductAttributes {
+interface RentalProductCreationAttributes extends Optional<RentalProductAttributes, 'id'> {}
+
+class RentalProduct extends Model<RentalProductAttributes, RentalProductCreationAttributes> implements RentalProductAttributes {
+  public id!: number;
   public rental_id!: number;
   public product_id!: number;
   public quantity!: number;
@@ -15,6 +19,11 @@ class RentalProduct extends Model<RentalProductAttributes> implements RentalProd
 
 RentalProduct.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
     rental_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
