d85bc90cbcd3cabd871160e9bfc014089b505080
franklin barco <franklin@flowrms.com>
2025-07-23 15:27:22 -0400

    Add client retrieval functionality: Implement getClients function to fetch all clients along with their rental count, and update client routes to include this new endpoint for improved client management.

--- src/controllers/client.controller.ts
+++ src/controllers/client.controller.ts
@@ -1,5 +1,31 @@
 import Client from '../models/client.model';
 import { RequestHandler } from 'express';
+import { Rental } from '../models';
+import { Sequelize } from 'sequelize';
+
+// Obtener todos los clientes
+export const getClients: RequestHandler = async (_req, res, next) => {
+  try {
+    const clients = await Client.findAll({
+      attributes: {
+        include: [
+          [Sequelize.fn('COUNT', Sequelize.col('rentals.id')), 'rentalCount']
+        ]
+      },
+      include: [
+        {
+          model: Rental,
+          as: 'rentals',
+          attributes: [], // No traemos los datos de las rentas, solo contamos
+        }
+      ],
+      group: ['Client.id']
+    });
+    res.status(200).json(clients);
+  } catch (error) {
+    next(error);
+  }
+};
 
 // Obtener un cliente por DNI
 export const getClientByDni: RequestHandler = async (_req, res, next) => {
--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { getClientByDni } from '../controllers/client.controller';
+import { getClientByDni, getClients } from '../controllers/client.controller';
 
 const router = Router();
 
 // Rutas públicas
+router.get('/', getClients);
 router.get('/:dni/dni', getClientByDni);
 
 export default router;
